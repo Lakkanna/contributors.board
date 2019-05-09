a87f6d0287b919959f8bdaa3e2f2d2e10a42e8f5
Lakkanna <lakkannawalikar@gmail.com>
2019-05-10 00:33:23 +0530

    Updates

---
+++ precache-manifest.a4a1ad11acfcb0808b36b53d89594b71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17a16a5650f7a8ab8dad11455ca97fb",
+    "url": "/contributors.board/index.html"
+  },
+  {
+    "revision": "a043c950d5bff6b0ba5c",
+    "url": "/contributors.board/static/css/main.27f90322.chunk.css"
+  },
+  {
+    "revision": "5666a7b593b9b2ac0731",
+    "url": "/contributors.board/static/js/2.d1b2fda4.chunk.js"
+  },
+  {
+    "revision": "a043c950d5bff6b0ba5c",
+    "url": "/contributors.board/static/js/main.3a3db9dc.chunk.js"
+  },
+  {
+    "revision": "4b9008923868f367ce57",
+    "url": "/contributors.board/static/js/runtime~main.b964f7fd.js"
+  }
+]);
