dd66f1a2365e03c01de5631e20f166ee625949be
Lakkanna <lakkannawalikar@gmail.com>
2019-05-10 12:52:32 +0530

    refactored typescript

--- src/api/searchRepositories.tsx
+++ src/api/searchRepositories.tsx
@@ -1,13 +1,21 @@
 
-const searchRepositories = async (language = "javascript", sortBy = "stars", orderBy = "desc", starsCount = 100, starsCondition = ">") => {
+type items = {
+  name: string
+}
+interface IResponse {
+  items: items[];
+  total_count: number
+}
+
+const searchRepositories = async (language = "javascript", sortBy = "stars",
+  orderBy = "desc", starsCount = 100, starsCondition = ">"): Promise<IResponse> => {
     let URL = "https://api.github.com/search/repositories";
     URL += `?q=stars:${starsCondition}${starsCount}+language:${language}&sort=${sortBy}&order=${orderBy}`;
-    console.log("URL fetching: ", URL);
 
     const repos = await fetch(URL);
     return repos.json().then(repo => {
-        console.log("Repositories ", repo);
-        return repo;
+      console.log("Repositories ", repo);
+      return repo;
     });
 };
 
